8cb524810caddd366b925dc1e39d5f86bf7f409b
danielsmetana7 <93688227+danielsmetana7@users.noreply.github.com>
2021-11-06 12:17:07 -0500

    DS_Update
    Created a project outline and reflected on the initial phases of the process

--- app.py
+++ app.py
@@ -16,62 +16,30 @@ st.title("Daniel Smetana Application")
 st.markdown("My name is Daniel Smetana, and this is my Streamlit Application..")
 st.markdown("This is v1")
 
-@st.cache(persist=True)
-def load_data():
-    df = pd.read_csv("https://datahub.io/machine-learning/iris/r/iris.csv")
-    return(df)
+st.title("Project Idea")
+st.markdown("My project idea is to create a restauarant recommender application.\
+The application will prompt the user with various questions (e.g. food type, \
+price range, distance from current location, restaurant style, etc) and then leverage\
+a model (likely a decision tree) to produce a recommendation to the user. \
+My initial thought is for the output to give the user three top options, giving them \
+some level of choice. The output would contain not only the restaurant name, but \
+also the location of the restaurant and potentially any dishes they are known for.")
+
+st.markdown("This project idea was spurred by the constant question of where to eat...\
+seemingly every weekend I struggle choosing between the numerous restaurants available,\
+and often would like someone (or something) to just decide for me. This application \
+looks to fill this need.")
 
 
+st.title("Reflection")
+st.markdown("I was able to produce a basic streamlit application shell that links to GitHub \
+(leveraging GitHub Desktop) and use Atom to edit my code. Obviously this is just the\
+start of the application, with the more technical aspects presenting potential \
+issue down the road - however, I feel comfortable with the overall idea and process.")
 
-def run():
-    st.subheader("Iris Data Loaded into a Pandas Dataframe.")
-
-    df = load_data()
-
-
-
-    disp_head = st.sidebar.radio('Select DataFrame Display Option:',('Head', 'All'),index=0)
-
-
-
-    #Multi-Select
-    #sel_plot_cols = st.sidebar.multiselect("Select Columns For Scatter Plot",df.columns.to_list()[0:4],df.columns.to_list()[0:2])
-
-    #Select Box
-    #x_plot = st.sidebar.selectbox("Select X-axis Column For Scatter Plot",df.columns.to_list()[0:4],index=0)
-    #y_plot = st.sidebar.selectbox("Select Y-axis Column For Scatter Plot",df.columns.to_list()[0:4],index=1)
-
-
-    if disp_head=="Head":
-        st.dataframe(df.head())
-    else:
-        st.dataframe(df)
-    #st.table(df)
-    #st.write(df)
-
-
-    #Scatter Plot
-    fig = px.scatter(df, x=df["sepallength"], y=df["sepalwidth"], color="class",
-                 size='petallength', hover_data=['petalwidth'])
-
-    fig.update_layout({
-                'plot_bgcolor': 'rgba(0, 0, 0, 0)'})
-
-    fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='lightgray')
-    fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgray')
-
-    st.write("\n")
-    st.subheader("Scatter Plot")
-    st.plotly_chart(fig, use_container_width=True)
-
-
-    #Add images
-    #images = ["<image_url>"]
-    #st.image(images, width=600,use_container_width=True, caption=["Iris Flower"])
-
-
-
-
-
-if __name__ == '__main__':
-    run()
+st.markdown("The tools that work for me currently are GitHub, Atom, Spyder, and Google Cloud.\
+I am unsure if Docker or Google SDK are working, but they were downloaded following the steps outlined.\
+Additionally, I redownloaded Visual Basic using the new link, but still have struggled to use it... \
+I am just not very familiar with the inteface, but from what I understand it is not necessary\
+to use the tool because of the alternatives avaiable? Please let me know if there would \
+be any concern with not using it.")
